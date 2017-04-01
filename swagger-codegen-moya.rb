93497dff04897163572d1c3fab8528e444ac9fba
Dang Thai Son <dangthaison.91@gmail.com>
2017-04-02 00:50:32 +0700

    - Update brew

--- swagger-codegen-moya.rb
+++ swagger-codegen-moya.rb
@@ -5,38 +5,16 @@
 class SwaggerCodegenMoya < Formula
   desc "Swagger Generator for Swift Moya "
   homepage "https://github.com/dangthaison91/swagger-codegen-moya/"
-  url "https://github.com/dangthaison91/swagger-codegen-moya/archive/2.2.3.tar.gz"
-  sha256 "828b0a66a31dd98ea5b0bbd1277116799c1dafb1d7e239f0405a5b1f36dda5e2"
+  url "https://github.com/dangthaison91/swagger-codegen-moya/raw/swift3_moya/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar"
+  sha256 "65a9d0e0a80528f0697851b2b81934bdbcfae2370725518755da2d85c63d5a93"
 
   depends_on :java => "1.7+"
-  depends_on "maven" => :build
+  # depends_on "maven" => :build
 
   def install
-      ENV.java_cache
-
+    #   ENV.java_cache
     #   system "mvn", "clean", "package", "-Dmaven.test.skip=true"
-      libexec.install "modules/swagger-codegen-cli/target/swagger-codegen-cli.jar"
-      bin.write_jar_script libexec/"swagger-codegen-cli.jar", "swagger-codegen-moya"
-
-    # Remove unrecognized options if warned by configure
-    # system "./configure", "--disable-debug",
-                        #   "--disable-dependency-tracking",
-                        #   "--disable-silent-rules",
-                        #   "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    # system "make", "install" # if this fails, try separate make/make install steps
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test swagger-codegen-moya`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    libexec.install "swagger-codegen-cli.jar"
+    bin.write_jar_script libexec/"swagger-codegen-cli.jar", "swagger-codegen-moya"
   end
 end
